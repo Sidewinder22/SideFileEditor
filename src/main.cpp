3e8ddba01abb281e7e999668473105eb7a2bacbe
Sidewinder22 <damian.stepien92@gmail.com>
2016-09-17 01:39:11 +0200

    Added main.cpp file

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+/**
+ * @author Damian Stępień
+ * @date 17.09.2016
+ */
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Hello, World!" << std::endl;
+
+
+    return 0;
+}
